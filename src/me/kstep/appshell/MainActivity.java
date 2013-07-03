355561804dba2f511f6c56350317858193e29c7f
Konstantin Stepanov <me@kstep.me>
2013-07-04 00:38:45 +0300

    use search view

--- src/me/kstep/appshell/MainActivity.java
+++ src/me/kstep/appshell/MainActivity.java
@@ -3,15 +3,17 @@ package me.kstep.appshell;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.ListView;
+import android.widget.SearchView;
 
-public class MainActivity extends Activity
+public class MainActivity extends Activity implements SearchView.OnQueryTextListener, SearchView.OnCloseListener
 {
 
     AppListAdapter adapter;
-    TextView searchView;
+    SearchView mSearchView;
 
     /** Called when the activity is first created. */
     @Override
@@ -23,18 +25,37 @@ public class MainActivity extends Activity
         ListView lv = (ListView) findViewById(R.id.search_result);
         adapter = new AppListAdapter(this);
         lv.setAdapter(adapter);
-
-        ActionBar ab = getActionBar();
-        ab.setCustomView(R.layout.actionbar);
-        ab.setDisplayShowCustomEnabled(true);
-        searchView = (TextView) ab.getCustomView().findViewById(R.id.search_text);
     }
 
-    public void onSearchButtonClick(View view)
-    {
-        CharSequence name = searchView.getText();
-        adapter.getFilter().filter(name);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem item = menu.add("Search");
+        item.setIcon(android.R.drawable.ic_menu_search);
+        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM|MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW);
+
+        mSearchView = new SearchView(this);
+        mSearchView.setOnQueryTextListener(this);
+        mSearchView.setOnCloseListener(this);
+        mSearchView.setIconifiedByDefault(true);
+        item.setActionView(mSearchView);
+
+        return true;
     }
 
+    @Override
+    public boolean onQueryTextChange(String query) {
+        adapter.getFilter().filter(query);
+        return true;
+    }
 
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return true;
+    }
+
+    @Override
+    public boolean onClose() {
+        mSearchView.setQuery(null, true);
+        return true;
+    }
 }
